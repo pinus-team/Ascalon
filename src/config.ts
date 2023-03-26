4c362199339855de62fa9dfe6d4f06a817580c26
Very Smooth Man <40175264+ManSmooth@users.noreply.github.com>
2023-03-26 15:42:31 +0700

    fixed connection string

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-	database_uri: `mongodb://${process.env.database_user}:${process.env.database_password}@${process.env.database_host}:${process.env.database_port}/${process.env.database_name}`
+	database_name : process.env.db_name,
+	database_uri: `mongodb://${process.env.database_user}:${process.env.database_password}@${process.env.database_host}:${process.env.database_port}/`
 };
