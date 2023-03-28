5c97072b98aca783c92ee7c0f93e185438fff458
Very Smooth Man <40175264+ManSmooth@users.noreply.github.com>
2023-03-29 02:31:56 +0700

    Added category query

--- src/services/aggregation.ts
+++ src/services/aggregation.ts
@@ -32,3 +32,43 @@ export const dish_join_pipeline = [
 		},
 	},
 ];
+
+export const dish_cat_join_pipeline = (cat: Number) => [
+	{
+		$match: {
+			category: cat,
+		},
+	},
+	{
+		$set: {
+			addons: {
+				$map: {
+					input: {
+						$map: {
+							input: "$addons",
+							in: {
+								$arrayElemAt: [
+									{
+										$objectToArray: "$$this",
+									},
+									1,
+								],
+							},
+						},
+					},
+					in: {
+						$toObjectId: "$$this.v",
+					},
+				},
+			},
+		},
+	},
+	{
+		$lookup: {
+			from: "addon",
+			localField: "addons",
+			foreignField: "_id",
+			as: "addons",
+		},
+	},
+];
