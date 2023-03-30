870bd381ae7c305c04ad438de2c8f88484f294e4
Very Smooth Man <40175264+ManSmooth@users.noreply.github.com>
2023-03-30 17:13:38 +0700

    bag functionality

--- src/routes/bag.ts
+++ src/routes/bag.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 import { Document, MongoError, ObjectId } from "mongodb";
+import { bag_join_pipeline } from "../services/aggregation";
 import { database } from "../services/database";
-import { bodyToBagItem, IBag, IBagItem } from "../types/bags";
-import { bodyToDish, IDish } from "../types/dish";
+import { bodyToBagItem, bodyToBagItemQuery, IBag } from "../types/bags";
 
 export default [
 	{
@@ -15,39 +15,43 @@ export default [
 		method: "post",
 		handler: postBagId,
 	},
+	{
+		path: "/del/:id",
+		method: "post",
+		handler: pullBagItem,
+	},
 ];
 
 function getBagId(req: Request, res: Response) {
-	res.status(200).send("getBagId");
-}
-
-function postBagId(req: Request, res: Response) {
-	const bagItem: IBagItem = bodyToBagItem(req.body);
 	database
 		.collection("bag")
-		.countDocuments({ user_id: new ObjectId(req.params.id) })
-		.then((count) => {
-			if (count === 0) {
-				database
-					.collection("bag")
-					.insertOne({
-						user_id: new ObjectId(req.params.id),
-						items: [],
-					})
-					.then((result) => {
-						res.status(200).send(result);
-					})
-					.catch((err: MongoError) => {
-						res.status(500).send(err);
-					});
-			}
+		.aggregate(bag_join_pipeline(new ObjectId(req.params.id)))
+		.toArray()
+		.then((docs: Document[]) => {
+			res.status(200).send(docs);
+		})
+		.catch((err: MongoError) => {
+			res.status(500).send(err);
 		});
+}
+
+async function postBagId(req: Request, res: Response) {
+	const bagItem: IBag = bodyToBagItem(req.params.id, req.body);
 	database
 		.collection<IBag>("bag")
-		.updateOne(
-			{ user_id: new ObjectId(req.params.id) },
-			{ $push: { items: bagItem } }
-		)
+		.insertOne(bagItem)
+		.then((result) => {
+			res.status(200).send(result);
+		})
+		.catch((err: MongoError) => {
+			res.status(500).send(err);
+		});
+}
+
+async function pullBagItem(req: Request, res: Response) {
+	database
+		.collection<IBag>("bag")
+		.deleteOne({ _id: new ObjectId(req.params.id) })
 		.then((result) => {
 			res.status(200).send(result);
 		})
--- src/types/bags.ts
+++ src/types/bags.ts
@@ -3,19 +3,33 @@ import { Double, Int32, ObjectId } from "mongodb";
 export interface IBag {
 	_id?: ObjectId;
 	user_id: ObjectId;
-	items: IBagItem[];
-}
-
-export interface IBagItem {
 	dish_id: ObjectId;
 	addons?: ObjectId[];
 	quantity: Int32;
+	note: string;
+}
+export interface IBagItemQuery {
+	dish_id: ObjectId;
+	addons?: ObjectId[];
+	note: string;
 }
 
-export function bodyToBagItem(body: any): IBagItem {
-	let obj: IBagItem = {
-		dish_id: new ObjectId(body.dish_id),
+export function bodyToBagItem(id: string, body: any): IBag {
+	let obj: IBag = {
+		user_id: new ObjectId(id),
+		dish_id: new ObjectId(body.foodId),
 		quantity: new Int32(body.quantity),
+		note: body.note,
+	};
+	if (body.addons)
+		obj.addons = body.addons.map((addon: any) => new ObjectId(addon));
+	return obj;
+}
+
+export function bodyToBagItemQuery(body: any): IBagItemQuery {
+	let obj: IBagItemQuery = {
+		dish_id: new ObjectId(body.foodId),
+		note: body.note,
 	};
 	if (body.addons)
 		obj.addons = body.addons.map((addon: any) => new ObjectId(addon));
